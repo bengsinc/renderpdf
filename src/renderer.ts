001b8c7bbc429f05d577884122a1166bdd060d29
bengsinc <111383298+bengsinc@users.noreply.github.com>
2022-11-29 17:00:19 -0300

    Mudança para pdf

--- src/renderer.ts
+++ src/renderer.ts
@@ -227,11 +227,10 @@ export class Renderer {
     }
 
     // Must be jpeg & binary format.
-    const screenshotOptions =
-      Object.assign({}, options, { type: 'jpeg', encoding: 'binary' });
+
     // Screenshot returns a buffer based on specified encoding above.
     // https://github.com/GoogleChrome/puppeteer/blob/v1.8.0/docs/api.md#pagescreenshotoptions
-    const buffer = await page.screenshot(screenshotOptions) as Buffer;
+    const buffer = await page.pdf({ format: 'A4' }) as Buffer;
     return buffer;
   }
 }
